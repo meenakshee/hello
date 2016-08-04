94b13c96fceabd4bf0b1f213b7e8bfbe750291bb
meenakshee <durby1892@gmail.com>
2016-08-04 00:25:00 -0700

    Initial commit

---
+++ app/src/main/java/com/example/minaxipc/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.example.minaxipc.hello;
+
+/**
+ * Created by MINAXI PC on 8/3/2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello sir my name is meenakshee");
+    }
+}
